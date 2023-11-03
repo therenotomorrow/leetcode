f60488d45ed90e48c7170a2c2f7b0f4c3a858a93
Kirill Kolesnikov <kkxnes@gmail.com>
2023-11-03 11:35:49 +0100

    Resolve day 3

---
+++ internal/solutions/buildarray.go
@@ -0,0 +1,17 @@
+package solutions
+
+func buildArray(target []int, n int) []string {
+	ops := make([]string, 0)
+	stream := 1
+
+	for _, num := range target {
+		for ; stream != num; stream++ {
+			ops = append(ops, "Push", "Pop")
+		}
+
+		ops = append(ops, "Push")
+		stream++
+	}
+
+	return ops
+}
---
+++ internal/solutions/buildarray_test.go
@@ -0,0 +1,29 @@
+package solutions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBuildArray(t *testing.T) {
+	type args struct {
+		target []int
+		n      int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{name: "smoke 1", args: args{target: []int{1, 3}, n: 3}, want: []string{"Push", "Push", "Pop", "Push"}},
+		{name: "smoke 2", args: args{target: []int{1, 2, 3}, n: 3}, want: []string{"Push", "Push", "Push"}},
+		{name: "smoke 3", args: args{target: []int{1, 2}, n: 4}, want: []string{"Push", "Push"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildArray(tt.args.target, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildArray() = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+}
