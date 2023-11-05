9f5339683b57b1a65089bf9d80839f4545d89d16
Kirill Kolesnikov <kkxnes@gmail.com>
2023-11-05 09:44:38 +0100

    Resolve 05

---
+++ internal/solutions/getwinner.go
@@ -0,0 +1,23 @@
+package solutions
+
+func getWinner(arr []int, k int) int {
+	winner := arr[0]
+	streak := 0
+
+	// at the end of arr we will find max
+	for _, opponent := range arr[1:] {
+		if streak == k {
+			break
+		}
+
+		if opponent < winner {
+			streak++
+			continue
+		}
+
+		winner = opponent
+		streak = 1
+	}
+
+	return winner
+}
