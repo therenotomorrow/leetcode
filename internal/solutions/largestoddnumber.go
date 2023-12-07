18d291d712609ec51cbf631f03c480aaf906ba0f
Kirill Kolesnikov <kkxnes@gmail.com>
2023-12-07 12:12:18 +0100

    7th of December

---
+++ internal/solutions/largestoddnumber.go
@@ -0,0 +1,11 @@
+package solutions
+
+func largestOddNumber(num string) string {
+	for i := len(num) - 1; i >= 0; i-- {
+		if num[i]%2 == 1 {
+			return num[:i+1]
+		}
+	}
+
+	return ""
+}
---
+++ internal/solutions/largestoddnumber_test.go
@@ -0,0 +1,25 @@
+package solutions
+
+import "testing"
+
+func TestLargestOddNumber(t *testing.T) {
+	type args struct {
+		num string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "smoke 1", args: args{num: "52"}, want: "5"},
+		{name: "smoke 2", args: args{num: "4206"}, want: ""},
+		{name: "smoke 3", args: args{num: "35427"}, want: "35427"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestOddNumber(tt.args.num); got != tt.want {
+				t.Errorf("largestOddNumber() = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+}
