7f3bef062da5cb1ee4cdab7114d13011e06e7fd9
Kirill Kolesnikov <kkxnes@gmail.com>
2023-12-19 08:53:42 +0100

    Add smooth image

---
+++ internal/solutions/imagesmoother_test.go
@@ -0,0 +1,27 @@
+package solutions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestImageSmoother(t *testing.T) {
+	type args struct {
+		img [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{name: "smoke 1", args: args{img: [][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}}, want: [][]int{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}},
+		{name: "smoke 2", args: args{img: [][]int{{100, 200, 100}, {200, 50, 200}, {100, 200, 100}}}, want: [][]int{{137, 141, 137}, {141, 138, 141}, {137, 141, 137}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := imageSmoother(tt.args.img); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("imageSmoother() = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+}
