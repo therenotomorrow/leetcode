bd0ee6eccf18dfad6c7e389be3289e8beecfb39a
Kirill Kolesnikov <kkxnes@gmail.com>
2023-12-27 08:15:28 +0100

    Add 27

---
+++ internal/solutions/mincost.go
@@ -0,0 +1,25 @@
+package solutions
+
+import "github.com/therenotomorrow/leetcode/pkg/mathfunc"
+
+func minCost(colors string, neededTime []int) int {
+	left := 0
+	right := 0
+	cost := 0
+
+	for left < len(colors) && right < len(colors) {
+		totalWindowCost := 0
+		maxWindowCost := 0
+
+		for right < len(colors) && colors[left] == colors[right] {
+			totalWindowCost += neededTime[right]
+			maxWindowCost = mathfunc.Max(maxWindowCost, neededTime[right])
+			right++
+		}
+
+		cost += totalWindowCost - maxWindowCost
+		left = right
+	}
+
+	return cost
+}
