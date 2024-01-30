b475812fab8b79390d98179be0ede2ffd48ab249
Kirill Kolesnikov <kkxnes@gmail.com>
2024-01-30 08:31:49 +0100

    Next please

---
+++ internal/solutions/evalrpn.go
@@ -0,0 +1,41 @@
+package solutions
+
+import (
+	"github.com/therenotomorrow/leetcode/pkg/datastruct"
+	"strconv"
+)
+
+func evalRPN(tokens []string) int {
+	var (
+		a, b, res int
+		stack     = datastruct.NewStack[int]()
+	)
+
+	for _, token := range tokens {
+		if val, err := strconv.Atoi(token); err == nil {
+			stack.Push(val)
+
+			continue
+		}
+
+		a, _ = stack.Pop()
+		b, _ = stack.Pop()
+
+		switch token {
+		case "+":
+			res = b + a
+		case "-":
+			res = b - a
+		case "*":
+			res = b * a
+		case "/":
+			res = b / a
+		}
+
+		stack.Push(res)
+	}
+
+	val, _ := stack.Peek()
+
+	return val
+}
