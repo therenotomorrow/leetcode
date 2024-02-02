de4dac60e5e4348222e3e2d60d3595f947250e71
Kirill Kolesnikov <kkxnes@gmail.com>
2024-02-02 16:38:51 +0100

    FosDem 2024

---
+++ internal/solutions/sequentialdigits_test.go
@@ -0,0 +1,31 @@
+package solutions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSequentialDigits(t *testing.T) {
+	type args struct {
+		low  int
+		high int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{name: "smoke 1", args: args{low: 100, high: 300}, want: []int{123, 234}},
+		{name: "smoke 2", args: args{low: 1000, high: 13000}, want: []int{1234, 2345, 3456, 4567, 5678, 6789, 12345}},
+		{name: "smoke 2", args: args{low: 10, high: 1000000000}, want: []int{1234, 2345, 3456, 4567, 5678, 6789, 12345}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sequentialDigits(tt.args.low, tt.args.high); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sequentialDigits() = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+}
