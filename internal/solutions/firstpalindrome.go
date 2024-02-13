005ae3e54a0e2e42fe198c44db2b280988735dc8
Kirill Kolesnikov <kkxnes@gmail.com>
2024-02-13 12:44:04 +0100

    We almost closed))

---
+++ internal/solutions/firstpalindrome.go
@@ -0,0 +1,23 @@
+package solutions
+
+func firstPalindrome(words []string) string {
+	for _, word := range words {
+		left := 0
+		right := len(word) - 1
+
+		for left < right {
+			if word[left] != word[right] {
+				break
+			}
+
+			left++
+			right--
+		}
+
+		if left >= right {
+			return word
+		}
+	}
+
+	return ""
+}
