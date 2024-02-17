f34ccbc86f562973dc7bf1ce7c3082e3d0a76f3e
Kirill Kolesnikov <kkxnes@gmail.com>
2024-02-17 18:53:10 +0100

    Daea

---
+++ internal/solutions/furthestbuilding.go
@@ -0,0 +1,51 @@
+package solutions
+
+import "container/heap"
+
+func furthestBuilding(heights []int, bricks int, ladders int) int {
+	peeks := new(IntHeap)
+
+	heap.Init(peeks)
+
+	for i := 0; i < len(heights)-1; i++ {
+		diff := heights[i+1] - heights[i]
+
+		if diff <= 0 {
+			continue // don't need to climb
+		}
+
+		heap.Push(peeks, diff)
+
+		if peeks.Len() <= ladders {
+			continue // could use ladder
+		}
+
+		// could replace ladder with min diff with bricks
+		bricks -= heap.Pop(peeks).(int)
+
+		if bricks < 0 {
+			return i
+		}
+	}
+
+	// we all in the end
+	return len(heights) - 1
+}
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
