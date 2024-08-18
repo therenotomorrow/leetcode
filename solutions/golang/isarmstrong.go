4f09d96e684aa7b587184d21b1d991ac9fc172f7
Kirill Kolesnikov <kkxnes@gmail.com>
2024-08-18 09:49:00 +0200

    Resolve

--- solutions/golang/isarmstrong.go
+++ solutions/golang/isarmstrong.go
@@ -1,8 +1,24 @@
 package golang
 
 func isArmstrong(n int) bool {
+	armstrong := 0
 	digits := make([]int, 0)
 
-	nCopy := n
+	for i := n; i > 0; {
+		digit := i % Digits
+		digits = append(digits, digit)
+		i /= Digits
+	}
 
+	for _, digit := range digits {
+		num := 1
+
+		for range len(digits) {
+			num *= digit
+		}
+
+		armstrong += num
+	}
+
+	return armstrong == n
 }
